d35bc934b34bc0b41dc08103c9fd13522a87335f
psterner <psterner@hotmail.com>
2023-11-05 22:09:53 +0100

    Add Hero component

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,13 @@
 import { Header } from "./Header";
+import { Hero } from "./components/Hero";
+import { PopularList } from "./components/PopularList";
 
 export const App = () => {
   return (
     <>
       <Header />
+      <Hero />
+      <PopularList />
     </>
   );
 };
