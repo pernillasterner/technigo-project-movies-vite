7a2c75ae5e5f440f82e76335ae1871c80c0c47d6
psterner <psterner@hotmail.com>
2023-11-06 13:52:09 +0100

    Add MovieDetail component

---
+++ src/components/MovieDetail/MovieDetail.jsx
@@ -0,0 +1,11 @@
+import { Link } from "react-router-dom";
+
+import "./MovieDetail.scss";
+
+export const MovieDetail = () => {
+  return (
+    <>
+      <h2>Movie Details Template</h2>
+    </>
+  );
+};
