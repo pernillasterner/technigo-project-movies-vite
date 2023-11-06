33c0ab12c6cc05b00770cf09c217c1d078c3d777
psterner <psterner@hotmail.com>
2023-11-06 21:12:00 +0100

    Update background image with data from Api

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -1,18 +1,18 @@
 import "./Hero.scss";
 
-export const Hero = () => {
+export const Hero = ({ popularMovies, secureBaseUrl, imageSize }) => {
+  console.log(popularMovies);
   return (
     <section
       className="hero"
       style={{
-        backgroundImage:
-          "url('https://images4.alphacoders.com/929/929236.jpg')",
+        backgroundImage: `url(${secureBaseUrl}${imageSize.backdrop_sizes[2]}${popularMovies[0].poster_path})`,
       }}
     >
       {/* Display if homepage */}
       <div className="movieContainer">
         <p className="movieBy">A MOVIE by Matthew</p>
-        <h1 className="movieTitle">venom</h1>
+        <h1 className="movieTitle">{popularMovies[0].title}</h1>
         <p className="storyBy">STORY INSPIRED BY SOMETHING OR SOMEBODY</p>
       </div>
       {/* Display if homepage */}
