7d0e85c92fad6e1683d93dc716300e8b24029852
psterner <psterner@hotmail.com>
2023-11-06 09:54:58 +0100

    Move header to App

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,13 +1,9 @@
-import { BrowserRouter, Routes, Route } from "react-router-dom";
-
-import { Header } from "../components/Header/Header";
 import { Hero } from "../components/Hero/Hero";
 import { MovieList } from "../components/MovieList/MovieList";
 
 export const Home = () => {
   return (
     <>
-      <Header />
       <Hero />
       <MovieList />
     </>
