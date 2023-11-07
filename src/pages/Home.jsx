ed3253a476da0fcc3baaf3a505bf2f4ef1d68385
psterner <psterner@hotmail.com>
2023-11-07 17:36:22 +0100

    Call Api functions instead of creating inside component. Select imageSizes

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,56 +1,49 @@
 /**
  * 1. Only pass the first or the most popular or latest movie to hero.
+ * 2. Set interval to the fetch data
+ * 3. Add loading indicator to the fetch request
  */
 import { useState, useEffect } from "react";
 import { Hero } from "../components/Hero/Hero";
 import { MovieList } from "../components/MovieList/MovieList";
+import { fetchPopularMovies, fetchImageSizes } from "../api/movieApi";
 
 export const Home = () => {
   const [popularMovies, setPopularMovies] = useState([]);
+  const [imageSizes, setImageSizes] = useState([]);
   const [secureBaseUrl, setSecureBaseUrl] = useState("");
-  const [imageSize, setImageSize] = useState([]);
-
-  // Time to fetch data from the server
-  const movieAPIKey = "f0cd5b88d931f2b23bac64656e5fda18";
-  const movieAPI = `https://api.themoviedb.org/3/movie/popular?api_key=${movieAPIKey}&language=en-US&page=1`;
-  const movieImgAPI = `https://api.themoviedb.org/3/configuration?api_key=${movieAPIKey}`;
 
   useEffect(() => {
-    const fetchAllPopularMovies = async () => {
+    const fetchData = async () => {
       try {
-        // Get all movies
-        const resPopularMovies = await fetch(movieAPI);
-        const popluarMoviesData = await resPopularMovies.json();
-        setPopularMovies(popluarMoviesData.results);
-        // Get all images
-        const resMovieImages = await fetch(movieImgAPI);
-        const movieImagesData = await resMovieImages.json();
-        setSecureBaseUrl(movieImagesData.images.secure_base_url);
-        setImageSize(movieImagesData.images);
-        // return data;
+        const [movies, images] = await Promise.all([
+          fetchPopularMovies(),
+          fetchImageSizes(),
+        ]);
+        setPopularMovies(movies);
+        setImageSizes(images);
+        setSecureBaseUrl(images.secure_base_url);
       } catch (err) {
-        console.error("Failed to fetch Popular Movies", err);
+        console.error("Error fetching data", err);
       }
     };
-    fetchAllPopularMovies();
+    // Calling function
+    fetchData();
   }, []);
 
   return (
     <>
-      {imageSize && imageSize.backdrop_sizes && (
-        <Hero
-          popularMovies={popularMovies}
-          secureBaseUrl={secureBaseUrl}
-          imageSize={imageSize}
-        />
-      )}
-      {imageSize && imageSize.poster_sizes && (
-        <MovieList
-          popularMovies={popularMovies}
-          secureBaseUrl={secureBaseUrl}
-          imageSize={imageSize}
-        />
-      )}
+      <Hero
+        movie={popularMovies}
+        secureBaseUrl={secureBaseUrl}
+        imageSizes={imageSizes}
+      />
+
+      <MovieList
+        popularMovies={popularMovies}
+        secureBaseUrl={secureBaseUrl}
+        imageSizes={imageSizes}
+      />
     </>
   );
 };
