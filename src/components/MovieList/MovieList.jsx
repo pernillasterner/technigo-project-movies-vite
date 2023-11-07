a9f604fafe1c0522f9b5da72095b7e3a32b51a15
psterner <psterner@hotmail.com>
2023-11-07 11:40:23 +0100

    Add wrapper around each movie to add background linear-gradient

--- src/components/MovieList/MovieList.jsx
+++ src/components/MovieList/MovieList.jsx
@@ -9,25 +9,25 @@ import "./MovieList.scss";
 export const MovieList = ({ popularMovies, secureBaseUrl, imageSize }) => {
   return (
     <section className="movieListContainer">
-      {/* should be able to change depending on category */}
-      <h2 className="title">most popular</h2>
-
+      <div className="titleContainer">
+        <h2 className="title">popular</h2>
+        <p className="subtitle">The most popular movies at the moment</p>
+      </div>
       {/* Add id to link */}
       {popularMovies.map((movie) => (
-        <>
+        <div key={movie.id} className="movieWrapper">
           <Link to={`/movie/${movie.id}`}>
             <img
               src={`${secureBaseUrl}${imageSize.poster_sizes[3]}${movie.poster_path}`}
               alt={movie.title}
               className="movieImg"
-              key={movie.id}
             />
             <div className="details">
               <h3 className="title">{movie.title}</h3>
               <p className="date">{movie.release_date}</p>
             </div>
           </Link>
-        </>
+        </div>
       ))}
     </section>
   );
