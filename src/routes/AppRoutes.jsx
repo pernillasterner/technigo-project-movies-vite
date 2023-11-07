7d20f36f2d7a556201ff6714828067c3d538f261
psterner <psterner@hotmail.com>
2023-11-07 19:36:57 +0100

    Create a seperate file for routes

---
+++ src/routes/AppRoutes.jsx
@@ -0,0 +1,14 @@
+import { Routes, Route } from "react-router-dom";
+import { Home } from "../pages/Home";
+import { MovieDetailPage } from "../pages/MovieDetailPage";
+import { NotFoundPage } from "../pages/NotFoundPage";
+
+export const AppRoutes = () => {
+  return (
+    <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="/movie/:movieId" element={<MovieDetailPage />} />
+      <Route path="*" element={<NotFoundPage />} />
+    </Routes>
+  );
+};
