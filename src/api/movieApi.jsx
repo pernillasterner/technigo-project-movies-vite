16edff6eff11ac433266f791f3c1e1a6ddc2b380
psterner <psterner@hotmail.com>
2023-11-08 12:00:23 +0100

    Fetch genres

--- src/api/movieApi.jsx
+++ src/api/movieApi.jsx
@@ -3,6 +3,7 @@ const API_KEY = "f0cd5b88d931f2b23bac64656e5fda18";
 const BASE__URL = "https://api.themoviedb.org/3/movie/popular?api_key=";
 const API_LANG = "&language=en-US&page=1";
 const IMAGE__URL = "https://api.themoviedb.org/3/configuration?api_key=";
+const GENRES__URL = "https://api.themoviedb.org/3/genre/movie/list?api_key=";
 
 // const apiKey = process.env.REACT_APP_API_KEY;
 // console.log(apiKey);
@@ -16,6 +17,7 @@ export const fetchPopularMovies = async () => {
     return data.results;
   } catch (err) {
     console.error("Error fetching popular movies", err);
+    throw err;
   }
 };
 
@@ -28,5 +30,19 @@ export const fetchImageSizes = async () => {
     return data.images;
   } catch (err) {
     console.error("Error fetching images sizes", err);
+    throw err;
+  }
+};
+
+// GENRES -> Movie List
+export const fetchMovieGenres = async () => {
+  const url = `${GENRES__URL}${API_KEY}`;
+  try {
+    const res = await fetch(url);
+    const data = await res.json();
+    return data;
+  } catch (err) {
+    console.error("Error fetching genres", err);
+    throw err;
   }
 };
