19687fdfdd4df51791c03c46e514528ee91ebee8
psterner <psterner@hotmail.com>
2023-11-08 20:21:33 +0100

    Add genre to hero and styling

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -1,24 +1,32 @@
+import { Link } from "react-router-dom";
 import { useLocation } from "react-router-dom";
 import "./Hero.scss";
 
-export const Hero = ({ title, posterPath }) => {
+export const Hero = ({ title, posterPath, details }) => {
   const isHomePage = location.pathname === "/";
   const baseUrl = "https://image.tmdb.org/t/p/w1280";
   const imageUrl = `${baseUrl}${posterPath}`;
   // const url = `https://image.tmdb.org/t/p/w1280/A4j8S6moJS2zNtRR8oWF08gRnL5.jpg`;
-
+  console.log(details);
   return (
     <section
       className="hero"
       style={{
-        backgroundImage: `url(${imageUrl})`,
+        backgroundImage: `linear-gradient(rgba(0, 0, 0, 0) 70%, rgb(0, 0, 0) 100%), url(${imageUrl})`,
       }}
     >
       {isHomePage && (
         <div className="movieContainer">
           <p className="movieBy">A MOVIE by Matthew</p>
           <h1 className="movieTitle">{title}</h1>
-          <p className="storyBy">STORY INSPIRED BY SOMETHING OR SOMEBODY</p>
+          <p className="storyBy">{details.tagline}</p>
+          <div className="genres">
+            {details.genres.map((genre) => (
+              <Link to={`/${genre.name.toLowerCase()}`} key={genre.id}>
+                {genre.name}
+              </Link>
+            ))}
+          </div>
         </div>
       )}
     </section>
