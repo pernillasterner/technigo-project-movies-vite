4d5a152aaa32119d5e6d66607e7fa481bd60b366
psterner <psterner@hotmail.com>
2023-11-08 09:03:26 +0100

    Use hardcoded path to hero image again

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -2,21 +2,22 @@ import { useLocation } from "react-router-dom";
 import "./Hero.scss";
 
 export const Hero = ({ popularMovies, secureBaseUrl, imageSizes }) => {
+  // console.log(movie);
   // const imageSize = imageSizes.heroSize;
-  const posterPath = popularMovies[0].poster_path;
+  // const posterPath = popularMovies[0].poster_path;
   // const baseUrl = secureBaseUrl;
   // const movieTitle = movie.title;
   // console.log(posterPath);
   // console.log(secureBaseUrl);
   // console.log(imageSizes.heroSize);
-  const url = `https://image.tmdb.org/t/p/w1280${posterPath}`;
+  // const url = `https://image.tmdb.org/t/p/w1280/A4j8S6moJS2zNtRR8oWF08gRnL5.jpg`;
   const isHomePage = location.pathname === "/";
   // backgroundImage: `url(${baseUrl}${imageSizes.heroSize}${popularMovies[0].posterPath})`,
   return (
     <section
       className="hero"
       style={{
-        backgroundImage: `url(${url})`,
+        backgroundImage: `url("https://image.tmdb.org/t/p/w1280/A4j8S6moJS2zNtRR8oWF08gRnL5.jpg")`,
       }}
     >
       {isHomePage && popularMovies.length > 0 && (
