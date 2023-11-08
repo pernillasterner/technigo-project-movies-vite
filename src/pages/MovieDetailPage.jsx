0a17b99ce84fef8b2c507c1ec721badd4eca1db1
psterner <psterner@hotmail.com>
2023-11-08 14:19:17 +0100

    Fetch data from movieDetails

--- src/pages/MovieDetailPage.jsx
+++ src/pages/MovieDetailPage.jsx
@@ -1,17 +1,45 @@
+import { useState, useEffect } from "react";
+import { useParams } from "react-router-dom";
+import { fetchMovieDetails } from "../api/movieApi";
+import { Hero } from "../components/Hero/Hero";
 import { MovieDetail } from "../components/MovieDetail/MovieDetail";
 
-export const MovieDetailPage = ({
-  popularMovies,
-  secureBaseUrl,
-  imageSizes,
-}) => {
+export const MovieDetailPage = () => {
+  const [movieDetails, setmMovieDetails] = useState({});
+  const [isLoading, setIsLoading] = useState(true);
+  const [error, setError] = useState(null);
+  const { movieId } = useParams();
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const movie = await fetchMovieDetails(movieId);
+        setmMovieDetails(movie);
+        if (movie) {
+          setIsLoading(false); // Data is loaded
+        }
+      } catch (err) {
+        setError(err.message);
+      }
+    };
+
+    if (isLoading) {
+      fetchData();
+    }
+  }, []);
+
+  if (isLoading) {
+    return <p>Loading...</p>;
+  }
+
+  if (error) {
+    return <p>Error: {error}</p>;
+  }
+  console.log(movieDetails);
   return (
     <>
-      <MovieDetail
-        popularMovies={popularMovies}
-        secureBaseUrl={secureBaseUrl}
-        imageSizes={imageSizes}
-      />
+      <Hero title={movieDetails.title} posterPath={movieDetails.poster_path} />
+      <MovieDetail movie={movieDetails} />
     </>
   );
 };
