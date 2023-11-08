66a338cded0aeb41037d835d7a1c9cfeadbd5dd9
psterner <psterner@hotmail.com>
2023-11-08 11:59:44 +0100

    Move fetching movies to home component

--- src/App.jsx
+++ src/App.jsx
@@ -1,77 +1,17 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import { useState, useEffect } from "react";
 // import { AppRoutes } from "./routes/AppRoutes";
-import { fetchPopularMovies, fetchImageSizes } from "./api/movieApi";
 import { Header } from "../src/components/Header/Header";
 import { Home } from "./pages/Home";
 import { MovieDetailPage } from "./pages/MovieDetailPage";
 import { NotFoundPage } from "./pages/NotFoundPage";
 
 export const App = () => {
-  const [popularMovies, setPopularMovies] = useState([]);
-  const [imageSizes, setImageSizes] = useState({
-    heroSize: "w1280",
-    posterSize: "w342",
-  });
-  const [secureBaseUrl, setSecureBaseUrl] = useState("");
-  const [isLoading, setIsLoading] = useState(true);
-
-  useEffect(() => {
-    const fetchData = async () => {
-      try {
-        const [movies, images] = await Promise.all([
-          fetchPopularMovies(),
-          fetchImageSizes(),
-        ]);
-        setPopularMovies(movies);
-        // setImageSizes(images);
-        setSecureBaseUrl(images.secure_base_url);
-        setIsLoading(false); // Data is loaded
-      } catch (err) {
-        console.error("Error fetching data", err);
-      }
-    };
-
-    // Calling function only once when component mounts
-    if (isLoading) {
-      fetchData();
-    }
-  }, [isLoading]);
-
-  // if (isLoading) {
-  //   return <p>Loading...</p>;
-  // }
-
-  // const mainProps = {
-  //   popularMovies: popularMovies,
-  //   imageSizes: imageSizes,
-  //   secureBaseUrl: secureBaseUrl,
-  // };
-
   return (
     <BrowserRouter>
       <Header />
       <Routes>
-        <Route
-          path="/"
-          element={
-            <Home
-              popularMovies={popularMovies}
-              imageSizes={imageSizes}
-              secureBaseUrl={secureBaseUrl}
-            />
-          }
-        />
-        <Route
-          path="/movie/:movieId"
-          element={
-            <MovieDetailPage
-              popularMovies={popularMovies}
-              imageSizes={imageSizes}
-              secureBaseUrl={secureBaseUrl}
-            />
-          }
-        />
+        <Route path="/" element={<Home />} />
+        <Route path="/movie/:movieId" element={<MovieDetailPage />} />
         <Route path="*" element={<NotFoundPage />} />
       </Routes>
     </BrowserRouter>
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,28 +1,46 @@
-/**
- * 1. Only pass the first or the most popular or latest movie to hero.✅
- * 2. Set interval to the fetch data
- * 3. Add loading indicator to the fetch request ✅
- */
-
+import React, { useState, useEffect } from "react";
+import { fetchPopularMovies } from "../api/movieApi";
 import { Hero } from "../components/Hero/Hero";
 import { MovieList } from "../components/MovieList/MovieList";
 
-export const Home = ({ popularMovies, secureBaseUrl, imageSizes }) => {
-  // console.log(popularMovies);
-  // console.log(secureBaseUrl);
-  // console.log(imageSizes);
+export const Home = () => {
+  const [selectedCat, setSelectedCat] = useState("popular");
+  const [movies, setMovies] = useState([]); // Initialize with an empty array
+  const [isLoading, setIsLoading] = useState(true);
+  const [error, setError] = useState(null);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const popularMovies = await fetchPopularMovies();
+        setMovies(popularMovies);
+        setIsLoading(false); // Data is loaded
+      } catch (err) {
+        setError(err.message);
+        setIsLoading(false);
+      }
+    };
+
+    if (isLoading) {
+      fetchData();
+    }
+  }, [isLoading]);
+
+  if (isLoading) {
+    return <p>Loading...</p>;
+  }
+
+  if (error) {
+    return <p>Error: {error}</p>;
+  }
+
+  const movieTitle = movies[0]?.title; // Use optional chaining to avoid errors
+  const posterPath = movies[0]?.poster_path; // Use optional chaining to avoid errors
+
   return (
     <>
-      <Hero
-        popularMovies={popularMovies}
-        secureBaseUrl={secureBaseUrl}
-        imageSizes={imageSizes}
-      />
-      <MovieList
-        popularMovies={popularMovies}
-        secureBaseUrl={secureBaseUrl}
-        imageSizes={imageSizes}
-      />
+      <Hero title={movieTitle} posterPath={posterPath} />
+      <MovieList movies={movies} selectedCat={selectedCat} />
     </>
   );
 };
