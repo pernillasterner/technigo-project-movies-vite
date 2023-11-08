4b5c0e9745498a850add7837db37dc1d121bb8ca
psterner <psterner@hotmail.com>
2023-11-08 12:01:09 +0100

    Change props to title and posterPath

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -1,29 +1,23 @@
 import { useLocation } from "react-router-dom";
 import "./Hero.scss";
 
-export const Hero = ({ popularMovies, secureBaseUrl, imageSizes }) => {
-  // console.log(movie);
-  // const imageSize = imageSizes.heroSize;
-  // const posterPath = popularMovies[0].poster_path;
-  // const baseUrl = secureBaseUrl;
-  // const movieTitle = movie.title;
-  // console.log(posterPath);
-  // console.log(secureBaseUrl);
-  // console.log(imageSizes.heroSize);
-  // const url = `https://image.tmdb.org/t/p/w1280/A4j8S6moJS2zNtRR8oWF08gRnL5.jpg`;
+export const Hero = ({ title, posterPath }) => {
   const isHomePage = location.pathname === "/";
-  // backgroundImage: `url(${baseUrl}${imageSizes.heroSize}${popularMovies[0].posterPath})`,
+  const baseUrl = "https://image.tmdb.org/t/p/w1280";
+  const imageUrl = `${baseUrl}${posterPath}`;
+  // const url = `https://image.tmdb.org/t/p/w1280/A4j8S6moJS2zNtRR8oWF08gRnL5.jpg`;
+
   return (
     <section
       className="hero"
       style={{
-        backgroundImage: `url("https://image.tmdb.org/t/p/w1280/A4j8S6moJS2zNtRR8oWF08gRnL5.jpg")`,
+        backgroundImage: `url(${imageUrl})`,
       }}
     >
-      {isHomePage && popularMovies.length > 0 && (
+      {isHomePage && (
         <div className="movieContainer">
           <p className="movieBy">A MOVIE by Matthew</p>
-          <h1 className="movieTitle">{popularMovies[0].title}</h1>
+          <h1 className="movieTitle">{title}</h1>
           <p className="storyBy">STORY INSPIRED BY SOMETHING OR SOMEBODY</p>
         </div>
       )}
