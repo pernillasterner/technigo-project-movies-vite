b3ca528b7bdb29151bd70f96524e95efe87c054f
psterner <psterner@hotmail.com>
2023-11-09 22:13:23 +0100

    Add accordian to production & lang

--- src/components/MovieDetail/MovieDetail.jsx
+++ src/components/MovieDetail/MovieDetail.jsx
@@ -6,13 +6,24 @@ import { Link } from "react-router-dom";
 import "./MovieDetail.scss";
 
 export const MovieDetail = ({ movie }) => {
-  // Search for a matching movie
-  // const movieMatch = popularMovies.find(
-  //   (movie) => movie.id === Number(movieId)
-  // );
-
   const baseUrl = "https://image.tmdb.org/t/p/w342";
   const imageUrl = `${baseUrl}${movie.poster_path}`;
+  const [isOpen, setIsOpen] = useState(false);
+
+  const handleAccordian = (e) => {
+    const h4 = e.target;
+    const ul = e.target.nextSibling;
+
+    if (!isOpen) {
+      h4.classList.add("is-open");
+      ul.style.display = "block";
+      setIsOpen(true);
+    } else {
+      h4.classList.remove("is-open");
+      ul.style.display = "none";
+      setIsOpen(false);
+    }
+  };
 
   return (
     <>
@@ -27,15 +38,13 @@ export const MovieDetail = ({ movie }) => {
           </p>
           <p className="description">{movie.overview}</p>
           <div className="moreInfo">
-            <h4>
-              <img src="../../assets/down-arrow.svg" alt="Arrow Down" />
-              More details
-            </h4>
+            <h4 onClick={handleAccordian}>production companies</h4>
             <ul className="prodCompany">
               {movie.production_companies.map((prod) => (
                 <li>{prod.name}</li>
               ))}
             </ul>
+            <h4 onClick={handleAccordian}>languages</h4>
             <ul className="lang">
               {movie.spoken_languages.map((lang) => (
                 <li>{lang.name}</li>
