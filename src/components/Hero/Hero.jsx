a771db313fffcdc623a0930e2952b213e556450f
psterner <psterner@hotmail.com>
2023-11-09 20:26:35 +0100

    Add message if movie is not found

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -1,10 +1,11 @@
 import { Link } from "react-router-dom";
+import { useParams } from "react-router-dom";
 import "./Hero.scss";
 
 export const Hero = ({ title, posterPath, details }) => {
-  const isHomePage = location.pathname === "/";
   const baseUrl = "https://image.tmdb.org/t/p/w1280";
   const imageUrl = `${baseUrl}${posterPath}`;
+  const { movieId } = useParams();
 
   return (
     <section
@@ -13,7 +14,7 @@ export const Hero = ({ title, posterPath, details }) => {
         backgroundImage: `linear-gradient(rgba(0, 0, 0, 0) 70%, rgb(0, 0, 0) 100%), url(${imageUrl})`,
       }}
     >
-      {isHomePage && (
+      {!movieId && title ? (
         <div className="movieContainer">
           <p className="movieBy">A MOVIE by Matthew</p>
           <h1 className="movieTitle">{title}</h1>
@@ -26,6 +27,14 @@ export const Hero = ({ title, posterPath, details }) => {
             ))}
           </div>
         </div>
+      ) : (
+        !title && (
+          <div className="no-movies">
+            <h1 className="">Oops!</h1>
+            <p className="">no movies found</p>
+            <Link to="/">home</Link>
+          </div>
+        )
       )}
     </section>
   );
