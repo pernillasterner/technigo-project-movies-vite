a78126d226fd750be9b903ed3ff53df7c3661abb
psterner <psterner@hotmail.com>
2023-11-12 16:05:38 +0100

    Add heroImg placeholder

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -1,38 +1,50 @@
 import { Link } from "react-router-dom";
 import { useParams } from "react-router-dom";
 import "./Hero.scss";
+import { NotFoundPage } from "../../pages/NotFoundPage";
 
 export const Hero = ({ title, posterPath, details }) => {
   const baseUrl = "https://image.tmdb.org/t/p/w1280";
-  const imageUrl = `${baseUrl}${posterPath}`;
+  let imageUrl = `${baseUrl}${posterPath}`;
   const { movieId } = useParams();
+  const notFoundContainer = imageUrl ? "" : "notFoundContainer";
 
   return (
     <section
-      className="hero"
+      className={`hero ${notFoundContainer}`}
       style={{
         backgroundImage: `linear-gradient(rgba(0, 0, 0, 0) 70%, rgb(0, 0, 0) 100%), url(${imageUrl})`,
       }}
     >
       {!movieId && title ? (
         <div className="movieContainer">
-          <p className="movieBy">A MOVIE by Matthew</p>
-          <h1 className="movieTitle">{title}</h1>
-          <p className="storyBy">{details.tagline}</p>
+          {/* <p className="movieBy">A MOVIE by Matthew</p> */}
+          {details.tagline ? (
+            <>
+              <h1 className="movieTitle">{details.tagline}</h1>
+              <p className="storyBy">{title}</p>
+            </>
+          ) : (
+            <h1 className="movieTitle">{title}</h1>
+          )}
           <div className="genres">
             {details.genres.map((genre) => (
-              <Link to={`/${genre.name.toLowerCase()}`} key={genre.id}>
+              <Link to={`/genre/${genre.id}`} key={genre.id}>
                 {genre.name}
               </Link>
             ))}
           </div>
         </div>
       ) : (
+        // TODO Fix this not found stuff
         !title && (
-          <div className="no-movies">
-            <h1 className="">Oops!</h1>
-            <p className="">no movies found</p>
-            <Link to="/">home</Link>
+          // <NotFoundPage info={setInfo} />
+          <div className="notFoundContainer">
+            <p>Oops!</p>
+            <h1>No movies found 🎬</h1>
+            <button className="movieBtn">
+              <Link to="/">🍿 home</Link>
+            </button>
           </div>
         )
       )}
