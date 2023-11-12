d07e18fa2459ff0d74caf88a29a24f1cf705dc51
psterner <psterner@hotmail.com>
2023-11-12 16:13:49 +0100

    Add loading spinner component

--- src/pages/MovieDetailPage.jsx
+++ src/pages/MovieDetailPage.jsx
@@ -1,9 +1,10 @@
 import { useState, useEffect } from "react";
-import { useParams, useNavigate } from "react-router-dom";
+import { useParams } from "react-router-dom";
 import { fetchMovieDetails } from "../api/movieApi";
 import { Hero } from "../components/Hero/Hero";
 import { MovieDetail } from "../components/MovieDetail/MovieDetail";
 import { NotFoundPage } from "./NotFoundPage";
+import { LoaderSpinner } from "../components/Loader/LoaderSpinner";
 
 export const MovieDetailPage = () => {
   const [movieDetails, setmMovieDetails] = useState({});
@@ -29,10 +30,6 @@ export const MovieDetailPage = () => {
     }
   }, [isLoading, movieId]);
 
-  if (isLoading) {
-    return <p>Loading...</p>;
-  }
-
   if (error) {
     return <p>Error: {error}</p>;
   }
@@ -43,8 +40,17 @@ export const MovieDetailPage = () => {
 
   return (
     <>
-      <Hero title={movieDetails.title} posterPath={movieDetails.poster_path} />
-      <MovieDetail movie={movieDetails} />
+      {isLoading ? (
+        <LoaderSpinner />
+      ) : (
+        <>
+          <Hero
+            title={movieDetails.title}
+            posterPath={movieDetails.poster_path}
+          />
+          <MovieDetail movie={movieDetails} />
+        </>
+      )}
     </>
   );
 };
